d139eab09969423f46d676a69de420a159a69224
Attajak Janrak <attajak@users.noreply.github.com>
2024-02-29 17:13:26 +0700

    Delete docs/_site directory

--- docs/_site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Hello World!",
-        "excerpt":"Hello World!   สวัสดีชาวโลก!  ","categories": ["Uncategoriesed"],
-        "tags": ["Hello","World","สวัสดี","ชาวโลก"],
-        "url": "/uncategoriesed/hello-world/",
-        "teaser": null
-      }]
